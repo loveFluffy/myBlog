5d1e0487d4492bb1c5e66f11512d7f94a7227e2d
loveFluffy <bliss159@gmail.com>
2018-08-28 15:48:29 +0800

    use iframe

--- javascripts/xscript.js
+++ javascripts/xscript.js
@@ -1,2 +1,17 @@
 // JavaScript source code
-$('#banner').load('index.html');
+
+/*<script src="xscript.js"></script>
+<script type="text/javascript">
+    function changeFrameHeight() {
+        var ifm = document.getElementById("dispframe");
+        ifm.height = document.documentElement.clientHeight;
+        /*ifm.scrolling = "no";
+    }
+window.onresize = function () {
+    changeFrameHeight();
+}
+        
+</script>
+<noscript>Your browser does not support JavaScript!</noscript>
+-->
+*/
